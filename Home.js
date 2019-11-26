4d59d74d440fa52d4e9682a0222d1980f2b2a078
Akshat Jain <akshatj0101@gmail.com>
2019-11-26 02:32:55 -0800

    location permissions for android and ios

--- Home.js
+++ Home.js
@@ -4,18 +4,49 @@ import Geolocation from '@react-native-community/geolocation';
 import SlidingUpPanel from 'rn-sliding-up-panel';
 import Map from './Map.js';
 import HomeV2 from './HomeV2.js'
+import {  PermissionsAndroid, Platform } from 'react-native'
 
 export default class Home extends Component{
   constructor(props) {
     super(props);
 
     this.updateLocation = this.updateLocation.bind(this)
+    this.requestLocationPermissionAndroid = this.requestLocationPermissionAndroid.bind(this)
     this.state = {};
-    this.updateLocation()
+
+    if(Platform.OS === 'android') {
+      this.requestLocationPermissionAndroid();
+    }
+    else {
+      this.updateLocation();
+    }
   }
 
   updateLocation = () => {
     Geolocation.getCurrentPosition(location => this.setState({ location }));
+  }
+
+  async requestLocationPermissionAndroid() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Berkeley Mobile Location Permission',
+          message:
+            'Berkeley Mobile would like to access your location.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        this.updateLocation()
+      } else {
+        console.log('Permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
   }
 
 
